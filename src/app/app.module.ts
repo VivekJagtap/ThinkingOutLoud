a894778edc4e02f928fb5feb9bf4b15f278a0c8d
VivekJagtap <vivekjagtap12@gmail.com>
2017-12-08 14:28:09 +0530

    Registration form added with validation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -36,7 +37,8 @@ const ROUTES = [
   imports: [
     BrowserModule,
     NgbModule.forRoot(),
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../Models/user.interface';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  public user:User;
 
   constructor() { }
 
   ngOnInit() {
+    this.user = {
+      username:'',
+      password:'',
+      email:''
+    }
   }
 
+  save(model:User,isValid:boolean){
+    console.log(model,isValid);
+  }
 }
